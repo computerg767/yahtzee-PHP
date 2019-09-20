eb7a7b4c585b9d33ff5a1522285971831dc30a83
Jacob <computerg767@gmail.com>
2019-09-20 18:34:01 -0400

    changes made on desktop

--- index.php
+++ index.php
@@ -4,23 +4,25 @@ session_start();
 $diceNames = ['one', 'two', 'three', 'four', 'five'];
 //For loop to redice lines of code
 $size_diceNames = sizeof($diceNames);
-for($i = 0; $i < $size_diceNames;$i++) {
-   $_SESSION['dice_' . $diceNames[$i]] = isset($_SESSION['dice_' . $diceNames[$i]])?$_SESSION['dice_' . $diceNames[$i]]:0;
+for ($i = 0; $i < $size_diceNames; ++$i) {
+    $_SESSION['dice_'.$diceNames[$i]] = isset($_SESSION['dice_'.$diceNames[$i]]) ? $_SESSION['dice_'.$diceNames[$i]] : 0;
 }
 //NUmber of rolls
 $_SESSION['rollNum'] = ((isset($_SESSION['rollNum'])) ? $_SESSION['rollNum'] : 0);
 
-$disable = "";
+$disable = '';
 // Funtions
 // Finding out checkedboxes to hold number
-function IsChecked($chkname,$value) {
-    if(!empty($_POST[$chkname])){
-        foreach($_POST[$chkname] as $chkval) {
-            if($chkval == $value){
+function IsChecked($chkname, $value)
+{
+    if (!empty($_POST[$chkname])) {
+        foreach ($_POST[$chkname] as $chkval) {
+            if ($chkval == $value) {
                 return true;
             }
         }
     }
+
     return false;
 }
 ?>
@@ -39,13 +41,31 @@ function IsChecked($chkname,$value) {
 
 <body>
 
- <h1>Alpha 1.2</h1>
+ <h1>Alpha 1.3</h1>
 
  <?php
 
 $diceNames = ['zero', 'one', 'two', 'three', 'four', 'five'];
 $diceNumbers = [0, 0, 0, 0, 0, 0];
 
+<<<<<<< HEAD
+if (isset($_POST['roll'])) {
+    ++$_SESSION['rollNum'];
+    // gets the size of the dies
+    $size_diceNames = sizeof($diceNames);
+    // goes through all the dies
+    for ($i = 1; $i < 6; ++$i) {
+        // adds the number to the die array if present
+        if (IsChecked('Dice', $i)) {
+            $diceNumbers[$i] = $_SESSION['dice_'.$diceNames[$i]];
+            echo 'This dice: '.'dice_'.$diceNames[$i].'<br>';
+        } else {
+            // creates new die number for new roll
+            $diceNumbers[$i] = rand(1, 6);
+            $_SESSION['dice_'.$diceNames[$i]] = $diceNumbers[$i];
+        }
+    }
+=======
 if(isset($_POST['roll'])){
    $_SESSION['rollNum']++;
    // gets the size of the dies
@@ -55,6 +75,7 @@ if(isset($_POST['roll'])){
       // adds the number to the die array if present
       if(IsChecked('Dice', $i)){
          $diceNumbers[$i] = $_SESSION['dice_' . $diceNames[$i]];
+         //line below for testing purposes
          echo "This dice: " . 'dice_' . $diceNames[$i] . "<br>";
       } else {
          // creates new die number for new roll
@@ -67,15 +88,21 @@ if(isset($_POST['reset'])){
 $_SESSION['rollNum'] = 0;
 for($i = 1; $i < 6 ;$i++){
    $diceNumbers[$i] = 0;
+>>>>>>> 9582781a29a23a3c5fd863263e33084bf371419f
 }
+if (isset($_POST['reset'])) {
+    $_SESSION['rollNum'] = 0;
+    for ($i = 1; $i < 6; ++$i) {
+        $diceNumbers[$i] = 0;
+    }
 }
 ?>
- <form method="post" action="<?php print $_SERVER['PHP_SELF']?>">
+ <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
   <table>
    <tr>
-    <?php  
-    for($i = 1; $i < 6;$i++) {
-         echo "<th>" . $diceNumbers[$i] . "</th>"; 
+    <?php
+    for ($i = 1; $i < 6; ++$i) {
+        echo '<th>'.$diceNumbers[$i].'</th>';
     }
     ?>
    </tr>
@@ -95,12 +122,12 @@ for($i = 1; $i < 6 ;$i++){
    </tr>
    <tr>
     <td>
-     <input type="submit" name="roll" value="Roll" <?=$disable?>>
+     <input type="submit" name="roll" value="Roll" <?=$disable; ?>>
      <input type="submit" name="reset" Value="Reset">
     </td>
    </tr>
   </table>
  </form>
-<?php echo ($_SESSION['rollNum']);?>
+<?php echo $_SESSION['rollNum']; ?>
 </body>
 </html>
